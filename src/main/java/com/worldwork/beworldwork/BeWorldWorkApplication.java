4b76636ff4d89d7947e3d645588baa8e58fbe5ea
TuanDiamond <tuanna612005@gmail.com>
2023-12-01 15:05:04 +0700

    Initial commit

---
+++ src/main/java/com/worldwork/beworldwork/BeWorldWorkApplication.java
@@ -0,0 +1,13 @@
+package com.worldwork.beworldwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeWorldWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeWorldWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/worldwork/beworldwork/BeWorldWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldwork.beworldwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeWorldWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
