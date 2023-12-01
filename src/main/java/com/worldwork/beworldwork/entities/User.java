e73b89c2cc5b30a4531a1cb3d90c4016db105414
TuanDiamond <tuanna612005@gmail.com>
2023-12-01 20:31:41 +0700

    Merge branch dev

--- src/main/java/com/worldwork/beworldwork/entities/User.java
+++ src/main/java/com/worldwork/beworldwork/entities/User.java
@@ -1,4 +1,20 @@
 package com.worldwork.beworldwork.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "jobs")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+
+    private String
 }
